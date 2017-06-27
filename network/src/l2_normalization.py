6e905fa550aff82a35dd731d69be673b3f87701c
Arjun Karpur <arjun.karpur@gmail.com>
2017-06-27 12:52:49 -0500

    Updated calc of l2 norm gradient. Much faster thanks to matrix foil trick (see .jpgs for work)

--- network/src/l2_normalization.py
+++ network/src/l2_normalization.py
@@ -18,48 +18,28 @@ class L2NormLayer(caffe.Layer):
       count += 1
       
   def backward(self, top, propagate_down, bottom):
-    top_diff = top[0].diff[...]
-    batch_size, feat_len = \
-      top_diff.shape[0], top_diff.shape[1]
+    batch_size = top[0].diff.shape[0]
 
     diff = np.zeros(bottom[0].diff.shape)
     for i in range(batch_size):
-      curr_top_diff = top_diff[i][...].reshape(1,feat_len)
-      in_vector = bottom[0].data[i][...]
-      jacobian = self.calc_l2norm_jacobian(in_vector)
-      diff[i][...] = curr_top_diff.dot(jacobian) # (1x1000)*(1000x1000)
+      diff[i] = \
+        self.calc_l2_norm_gradient( \
+          bottom[0].data[i], top[0].data[i], top[0].diff[i] \
+        )
     bottom[0].diff[...] = diff
-
-  def calc_l2norm_jacobian(self, X):
-    n = len(X)
-    jacobian = np.zeros((n,n))
-    for row in range(len(jacobian)):
-      for col in range(len(jacobian[0])):
-        jacobian[row, col] = self.dFi_Xj(X, row, col)
-    return jacobian
-  
-  def dFi_Xj(self, X, i, j):
-    # F_i(X) = X_i/(||X||) = X_i/G(X)
-    val = 0
-    if i == j:
-      val = \
-        (self.G_X(X) - (X[i]*self.dG_Xj(X,j))) / \
-        (self.G_X(X) * self.G_X(X))
-    else:
-      val = \
-        X[i] * \
-        (-1/(self.G_X(X)*self.G_X(X))) * \
-        self.dG_Xj(X, j)
-    return val
-
-  def G_X(self, X):
-    # Vector norm function (G(X) = ||X||)
-    return np.sqrt(X.dot(X))
-  
-  def dG_Xj(self, X, j):
-    gradient = \
-      0.5 * (1/np.sqrt(X.dot(X))) * (2*X[j])
-    return gradient
 
   def reshape(self, bottom, top):
     pass
+
+  def calc_l2_norm_gradient(self, v, v_bar, dFdvbar):
+    # See 'network/l2_norm_gradient.jpg' for formulation
+    v = v.reshape(v.size, 1)
+    v_bar = v_bar.reshape(v_bar.size,1)
+    v_bar_t = v_bar.reshape(1, v_bar.size)
+    dFdvbar = dFdvbar.reshape(dFdvbar.size,1)
+
+    num_one = dFdvbar
+    num_two = v_bar.dot(v_bar_t.dot(dFdvbar))
+    denom = np.sqrt(v.transpose().dot(v))
+    result = (num_one - num_two)/denom
+    return result.reshape(1, v_bar.size)

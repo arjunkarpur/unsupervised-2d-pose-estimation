a2883bbdaa5628a7d262eb5217aeefc0f11869bf
Arjun Karpur <arjun.karpur@gmail.com>
2017-07-11 15:23:58 -0500

    New triplet generation technique

--- network/src/train_config.py
+++ network/src/train_config.py
@@ -5,7 +5,7 @@ REAL_IMAGE_PATH = "/mnt/localscratch/arjun/real_image_dump"
 RENDERED_IMAGE_PATH = "/mnt/localscratch/arjun/renderings_210/healthy"
 SOLVER_PROTO = "../solver.prototxt"
 PRETRAIN_WEIGHTS_FP = "../models/bvlc_reference_caffenet.caffemodel"
-TRIPLETS_FP = "../../triplet_generation/out/triplets_shuffle.txt"
+TRIPLETS_FP = "../../triplet_generation/out/triplets_four/triplets_shuffle.txt"
 
 # Network parameters
 GPU = True
--- triplet_generation/generate_triplets.py
+++ triplet_generation/generate_triplets.py
@@ -1,10 +1,11 @@
+import sys
 import h5py
 import random
 
 # Inputs vars
 dist_grids_fp = "/net/cvcfs/storage/skull-atlas/imgscrape/dist_grids/alexnet_fc7_dist_grids.hdf5"
-poses_fp = "../pose_estimation/out/alexnet_fc7_poses.txt"
-out_fp = "./out/triplets.txt"
+poses_fp = "../pose_estimation/out/split/%i.txt" % int(sys.argv[1])
+out_fp = "./out/triplets_four/split/triplets_%i.txt" % int(sys.argv[1])
 num_positives = 50
 negs_per_pos = 4
 
@@ -26,8 +27,8 @@ out_f = open(out_fp, 'w')
 
 # Bins for selecting triplets
 num_bins = 5
-pos_bin_probs = [.50, .20, .15, .10, .05]
-neg_bin_probs = [.05, .10, .15, .20, .50]
+pos_bin_probs = [.60, .20, .10, .07, .03]
+neg_bin_probs = [.03, .07, .10, .20, .60]
 pos_bin_bag = []
 neg_bin_bag = []
 for i in range(len(pos_bin_probs)):
@@ -54,13 +55,43 @@ for im in im_poses:
       all_dists.append((curr_name, curr_pose, curr_dist))
   sorted_dists = sorted(all_dists, key=lambda x: float(x[2]))
 
-  #TODO: create bins
+  # Create bins
+  bin_sizes = []
+  for i in range(num_bins):
+    if i == (num_bins-1):
+      size = len(sorted_dists) / num_bins
+    else:
+      size = (len(sorted_dists) / num_bins) + (len(sorted_dists) % num_bins)
+    bin_sizes.append(size)
 
+  """
+  # BIN INFO PRINTING
+  for b in range(len(bin_sizes)):
+    print "BIN %i" % b
+    print "start: ", sorted_dists[b*bin_sizes[0]]
+    print "end: ", sorted_dists[b*bin_sizes[0] + bin_sizes[b] - 1]
+  """
+
+  # Generate triplets
   for i in range(num_positives):
-    pass
-    #TODO: pick positive
-    #TODO: pick #NEGS_PER_POS negatives
-    #TODO: write triplets
+    anchor = name
+    # Pick 1 positive and #NEGS_PER_POS negatives
+    pos_bin = pos_bin_bag[random.randint(0,99)]
+    bin_size = bin_sizes[pos_bin]
+    bin_ind = random.randint(0,bin_size-1)
+    positive = sorted_dists[pos_bin*bin_sizes[0] + bin_ind]
+    pos_name = "rend%i_%s" % (positive[1], str(positive[0]))
+
+    # Pick #NEGS_PER_POS negatives
+    for j in range(negs_per_pos):
+      neg_bin = neg_bin_bag[random.randint(0,99)]
+      bin_size = bin_sizes[neg_bin]
+      bin_ind = random.randint(0, bin_size-1)
+      negative = sorted_dists[neg_bin*bin_sizes[0] + bin_ind]
+
+      neg_name = "rend%i_%s" % (negative[1], str(negative[0]))
+      triplet = "%s,%s,%s" % (anchor, pos_name, neg_name)
+      out_f.write("%s\n" % triplet)
 
   """
   positives = dist_grids_f[name][pose][:top_n]
